e12486cd364133ec68f43a094379adce7385d14e
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-01-06 13:10:38 +0000

    Update run.ps1

--- Ms365-ListGroups/run.ps1
+++ Ms365-ListGroups/run.ps1
@@ -2,49 +2,34 @@
 
 .SYNOPSIS
     
-    This function is used to remove a user from a distribution group in Microsoft 365.
+    This function lists all MS Teams in Microsoft 365.
 
 .DESCRIPTION
-            
-    This function is used to remove a user from a distribution group in Microsoft 365.
-        
+    
+    This function lists all MS Teams in Microsoft 365.
+    
     The function requires the following environment variables to be set:
-        
+    
     Ms365_AuthAppId - Application Id of the service principal
     Ms365_AuthSecretId - Secret Id of the service principal
     Ms365_TenantId - Tenant Id of the Microsoft 365 tenant
     SecurityKey - Optional, use this as an additional step to secure the function
-        
+ 
     The function requires the following modules to be installed:
-       
+    
     Microsoft.Graph
-
+    
 .INPUTS
 
-    UserEmail - user email address that exists in the tenant
-    GroupName - group name that exists in the tenant
     TenantId - string value of the tenant id, if blank uses the environment variable Ms365_TenantId
-    TicketId - optional - string value of the ticket id used for transaction tracking
     SecurityKey - Optional, use this as an additional step to secure the function
-
-    JSON Structure
-
-    {
-        "UserEmail": "email@address.com",
-        "GroupName": "Group Name",
-        "TenantId": "12345678-1234-1234-123456789012",
-        "TicketId": "123456,
-        "SecurityKey", "optional"
-    }
 
 .OUTPUTS
 
     JSON response with the following fields:
 
-    Message - Descriptive string of result
-    TicketId - TicketId passed in Parameters
-    ResultCode - 200 for success, 500 for failure
-    ResultStatus - "Success" or "Failure"
+    Id - The ID of the MS Team
+    Name - The display name of the MS Team
 
 #>
 
@@ -52,36 +37,17 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-Write-Host "Add User to Group function triggered."
+Write-Host "ListTeams function triggered."
 
 $resultCode = 200
 $message = ""
 
-$UserEmail = $Request.Body.UserEmail
-$GroupName = $Request.Body.GroupName
 $TenantId = $Request.Body.TenantId
-$TicketId = $Request.Body.TicketId
 $SecurityKey = $env:SecurityKey
 
 if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
     Write-Host "Invalid security key"
     break;
-}
-
-if (-Not $userEmail) {
-    $message = "UserEmail cannot be blank."
-    $resultCode = 500
-}
-else {
-    $UserEmail = $UserEmail.Trim()
-}
-
-if (-Not $groupName) {
-    $message = "GroupName cannot be blank."
-    $resultCode = 500
-}
-else {
-    $GroupName = $GroupName.Trim()
 }
 
 if (-Not $TenantId) {
@@ -91,65 +57,43 @@ else {
     $TenantId = $TenantId.Trim()
 }
 
-if (-Not $TicketId) {
-    $TicketId = ""
-}
-
-Write-Host "User Email: $UserEmail"
-Write-Host "Group Name: $GroupName"
 Write-Host "Tenant Id: $TenantId"
-Write-Host "Ticket Id: $TicketId"
 
-if ($resultCode -Eq 200)
-{
+if ($resultCode -Eq 200) {
     $secure365Password = ConvertTo-SecureString -String $env:Ms365_AuthSecretId -AsPlainText -Force
     $credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365_AuthAppId, $secure365Password)
 
     Connect-MgGraph -ClientSecretCredential $credential365 -TenantId $TenantId
 
-    $GroupObject = Get-MgGroup -Filter "displayName eq '$GroupName'"
+    $teams = Get-MgGroup -Filter "resourceProvisioningOptions/Any(x:x eq 'Team')"
 
-    Write-Host $GroupObject.DisplayName
-    Write-Host $GroupObject.Id
-
-    $UserObject = Get-MgUser -Filter "userPrincipalName eq '$UserEmail'"
-
-    Write-Host $UserObject.userPrincipalName
-    Write-Host $UserObject.Id
-
-    if (-Not $GroupObject) {
-        $message = "Request failed. Group `"$GroupName`" could not be found to add user `"$UserEmail`" to."
+    if (-Not $teams) {
+        $message = "No MS Teams found."
         $resultCode = 500
     }
 
-    if (-Not $UserObject) {
-        $message = "Request failed. User `"$UserEmail`" not be found to add to group `"$GroupName`"."
-        $resultCode = 500
+    $teamsList = $teams | ForEach-Object {
+        [PSCustomObject]@{
+            Id   = $_.Id
+            Name = $_.DisplayName
+        }
     }
-
-    $GroupMembers = Get-MgGroupMember -GroupId $GroupObject.Id
-
-    if ($GroupMembers.Id -NotContains $UserObject.Id) {
-        $message = "Request failed. User `"$UserEmail`" is not a member of group `"$GroupName`"."
-        $resultCode = 500
-    } 
 
     if ($resultCode -Eq 200) {
-        Remove-MgGroupMember -GroupId $GroupObject.Id -DirectoryObjectId $UserObject.Id
-        $message = "Request completed. `"$UserEmail`" has been removed from group `"$GroupName`"."
+        $message = "Request completed. MS Teams listed successfully."
     }
 }
 
 $body = @{
-    Message = $message
-    TicketId = $TicketId
-    ResultCode = $resultCode
+    Message      = $message
+    Teams        = $teamsList
+    ResultCode   = $resultCode
     ResultStatus = if ($resultCode -eq 200) { "Success" } else { "Failure" }
 } 
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
-    ContentType = "application/json"
-})
+        StatusCode  = [HttpStatusCode]::OK
+        Body        = $body
+        ContentType = "application/json"
+    })
