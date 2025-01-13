2d332d3c11fd3dcf0b337a976dd5f42fd39b5f03
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-01-13 14:03:08 +0000

    Update run.ps1

--- Ms365-ListLicenses/run.ps1
+++ Ms365-ListLicenses/run.ps1
@@ -1,36 +1,43 @@
 <# 
 
 .SYNOPSIS
-    
+
     This function lists all available Microsoft 365 licenses.
 
 .DESCRIPTION
-    
+
     This function lists all available Microsoft 365 licenses.
     
     The function requires the following environment variables to be set:
     
-    Ms365_AuthAppId - Application Id of the service principal
-    Ms365_AuthSecretId - Secret Id of the service principal
-    Ms365_TenantId - Tenant Id of the Microsoft 365 tenant
+    Ms365_AuthAppId - Application Id of the Azure AD application
+    Ms365_AuthSecretId - Secret Id of the Azure AD application
+    Ms365_TenantId - Tenant Id of the Azure AD application
     SecurityKey - Optional, use this as an additional step to secure the function
- 
+
     The function requires the following modules to be installed:
     
-    Microsoft.Graph
-    
+    Microsoft.Graph     
+
 .INPUTS
 
+    CompanyId - numeric company id
     TenantId - string value of the tenant id, if blank uses the environment variable Ms365_TenantId
-    TicketId - optional - string value of the ticket id used for transaction tracking
     SecurityKey - Optional, use this as an additional step to secure the function
+
+    JSON Structure
+
+    {
+        "CompanyId": "12"
+        "TenantId": "12345678-1234-1234-1234-123456789012",
+        "SecurityKey", "optional"
+    }
 
 .OUTPUTS
 
     JSON response with the following fields:
 
-    SkuId - The SKU Id of the license
-    SkuPartNumber - The part number of the license
+    Message - Descriptive string of result
     TicketId - TicketId passed in Parameters
     ResultCode - 200 for success, 500 for failure
     ResultStatus - "Success" or "Failure"
@@ -41,13 +48,40 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-Write-Host "ListLicenses function triggered."
+function Set-CompanyM365License {
+    param (
+        [string]$Token,
+        [string]$AppId,
+        [string]$SecretId,
+        [int]$CompanyId,
+        [string]$LicenseList
+    )
 
-$resultCode = 200
-$message = ""
+    # Construct the basic authentication header
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${AppId}:${SecretId}"))
+    $headers = @{
+        "Authorization" = "Basic $base64AuthInfo"
+        "Content-Type" = "application/json"
+    }
 
-$TenantId = $Request.Body.TenantId
-$TicketId = $Request.Body.TicketId
+    $body = @{
+        "companyId" = $CompanyId
+        "token" = "$Token"
+        "value" = "$LicenseList"
+    }
+
+    $bodyJson = $body | ConvertTo-Json
+
+    # Replace the following URL with the actual REST API endpoint
+    $apiUrl = "https://api.us.cloudradial.com/api/beta/token"
+
+    $response = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Body $bodyJson -Method Post
+
+    Write-Host "API response: $($response | ConvertTo-Json -Depth 4)"
+}
+
+$companyId = $Request.Body.CompanyId
+$tenantId = $Request.Body.TenantId
 $SecurityKey = $env:SecurityKey
 
 if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
@@ -55,48 +89,44 @@ if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
     break;
 }
 
-if (-Not $TenantId) {
-    $TenantId = $env:Ms365_TenantId
-}
-else {
-    $TenantId = $TenantId.Trim()
+if (-Not $companyId) {
+    $companyId = 1
 }
 
-if (-Not $TicketId) {
-    $TicketId = ""
+if (-Not $tenantId) {
+    $tenantId = $env:Ms365_TenantId
 }
 
-Write-Host "Tenant Id: $TenantId"
-Write-Host "Ticket Id: $TicketId"
+$resultCode = 200
+$message = ""
 
-if ($resultCode -Eq 200) {
-    $secure365Password = ConvertTo-SecureString -String $env:Ms365_AuthSecretId -AsPlainText -Force
-    $credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365_AuthAppId, $secure365Password)
+$secure365Password = ConvertTo-SecureString -String $env:Ms365_AuthSecretId -AsPlainText -Force
+$credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365_AuthAppId, $secure365Password)
 
-    Connect-MgGraph -ClientSecretCredential $credential365 -TenantId $TenantId
+Connect-MgGraph -ClientSecretCredential $credential365 -TenantId $tenantId
 
-    $licenses = Get-MgSubscribedSku
+# Get all licenses in the tenant
+$licenses = Get-MgSubscribedSku
 
-    if (-Not $licenses) {
-        $message = "No licenses found."
-        $resultCode = 500
-    }
-
-    $licensesList = $licenses | ForEach-Object {
-        [PSCustomObject]@{
-            SkuId         = $_.SkuId
-            SkuPartNumber = $_.SkuPartNumber
-        }
-    }
-
-    if ($resultCode -Eq 200) {
-        $message = "Request completed. Licenses listed successfully."
+# Extract license details
+$licenseDetails = $licenses | ForEach-Object {
+    [PSCustomObject]@{
+        SkuId         = $_.SkuId
+        SkuPartNumber = $_.SkuPartNumber
     }
 }
+
+# Convert the array of license details to a comma-separated string
+$licenseDetailsString = $licenseDetails -join ","
+
+Set-CompanyM365License -Token "CompanyM365License" -AppId ${env:CloudRadialCsa_ApiPublicKey} -SecretId ${env:CloudRadialCsa_ApiPrivateKey} -CompanyId $companyId -LicenseList $licenseDetailsString
+
+Write-Host "Updated CompanyM365License for Company Id: $companyId."
+
+$message = "Company licenses for $companyId have been updated."
 
 $body = @{
-    Message      = $message + $licensesList
-    Licenses     = $licensesList
+    Message      = $message
     TicketId     = $TicketId
     ResultCode   = $resultCode
     ResultStatus = if ($resultCode -eq 200) { "Success" } else { "Failure" }
