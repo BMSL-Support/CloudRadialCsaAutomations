36f8622457f321f30ce0a25b2e058f070d8db328
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-02-06 11:01:20 +0000

    Update run.ps1

--- Connectwise-SimpleTicketNote/run.ps1
+++ Connectwise-SimpleTicketNote/run.ps1
@@ -1,46 +1,31 @@
 <# 
-
 .SYNOPSIS
-    
     This function is used to add a note to a ConnectWise ticket.
-
 .DESCRIPTION
-                    
     This function is used to add a note to a ConnectWise ticket.
-                    
     The function requires the following environment variables to be set:
-                    
     ConnectWisePsa_ApiBaseUrl - Base URL of the ConnectWise API
     ConnectWisePsa_ApiCompanyId - Company Id of the ConnectWise API
     ConnectWisePsa_ApiPublicKey - Public Key of the ConnectWise API
     ConnectWisePsa_ApiPrivateKey - Private Key of the ConnectWise API
     ConnectWisePsa_ApiClientId - Client Id of the ConnectWise API
     SecurityKey - Optional, use this as an additional step to secure the function
-                    
     The function requires the following modules to be installed:
-                   
-    None        
-
+    ConnectWiseManageAPI
 .INPUTS
-
     TicketId - string value of numeric ticket number
     Message - text of note to add
     Internal - boolean indicating whether not should be internal only
     SecurityKey - optional security key to secure the function
-
     JSON Structure
-
     {
         "TicketId": "123456",
         "Message": "This is a note",
         "Internal": true,
         "SecurityKey": "optional"
     }
-
 .OUTPUTS
-    
     JSON structure of the response from the ConnectWise API
-
 #>
 
 using namespace System.Net
@@ -55,41 +40,31 @@ function Add-ConnectWiseTicketNote {
         [string]$ClientId,
         [string]$TicketId,
         [string]$Text,
-        [boolean]$Internal = $false
-        )
+        [boolean]$Internal = $true
+    )
 
-    # Construct the API endpoint for adding a note
-    $apiUrl = "$ConnectWiseUrl/v4_6_release/apis/3.0/service/tickets/$TicketId/notes"
-    Write-Host "API URL: $apiUrl"
+    # Import the ConnectWiseManageAPI module
+    Import-Module ConnectWiseManageAPI
 
-    # Create the note serviceObject
-    $notePayload = @{
-        ticketId = $TicketId
-        text = $Text
-        detailDescriptionFlag = $true
-        internalAnalysisFlag = $Internal
-        #resolutionFlag = $false
-        #customerUpdatedFlag = $false 
+    # Set up the ConnectWise API connection
+    $cwParams = @{
+        ConnectWiseUrl = $ConnectWiseUrl
+        PublicKey = $PublicKey
+        PrivateKey = $PrivateKey
+        ClientId = $ClientId
     }
-    $Body = ConvertTo-Json $notePayload
-    Write-Host "Note Payload: $Body"
-    
-    # Set up the authentication headers
-    $AuthString  = "$($ClientId)+$($PublicKey):$($PrivateKey)"
-    $EncodedAuth  = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($AuthString));
-    $headers = @{
-        Authorization = "Basic $EncodedAuth"
-        ClientID = $ClientID
-        'Cache-Control'= 'no-cache'
-        ConnectionMethod = 'Key'
-        Accept = "application/vnd.connectwise.com+json; version=v2024_1"
-    }
-    Write-Host "Headers: $headers"
+    Connect-CWManage @cwParams
 
-    # Make the API request to add the note
-    $result = Invoke-WebRequest -Uri $apiUrl -Method 'Post' -Headers $headers -Body $Body
-    Write-Host "API Response: $result"
-    return $result.content
+    # Create the note object
+    $noteParams = @{
+        TicketId = $TicketId
+        Text = $Text
+        Internal = $Internal
+    }
+    $result = Add-CWManageTicketNote @noteParams
+
+    Write-Host $result
+    return $result
 }
 
 $TicketId = $Request.Body.TicketId
@@ -126,4 +101,14 @@ $result = Add-ConnectWiseTicketNote -ConnectWiseUrl $env:ConnectWisePsa_ApiBaseU
     -Text $Text `
     -Internal $Internal
 
-Write-Host "Final Result: $result"
+Write-Host $result.Message
+
+$body = @{
+    response = ($result | ConvertTo-Json);
+} 
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+    ContentType = "application/json"
+})
