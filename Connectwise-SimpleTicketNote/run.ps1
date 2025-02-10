babc1508339770a99e53efb69bb3118675b6fc5e
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-02-10 10:55:53 +0000

    Update run.ps1

--- Connectwise-SimpleTicketNote/run.ps1
+++ Connectwise-SimpleTicketNote/run.ps1
@@ -83,7 +83,7 @@ $note = @{
 $json = $note | ConvertTo-Json
 
 # Set up the API request
-$apiUrl = "${env:ConnectWisePsa_ApiBaseUrl}/service/tickets/$ticketId/notes"
+$apiUrl = "${env:ConnectWisePsa_ApiBaseUrl}/v4_6_release/apis/3.0/service/tickets/$ticketId/notes"
 $authHeader = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${env:ConnectWisePsa_ApiCompanyId}+${env:ConnectWisePsa_ApiPublicKey}:${env:ConnectWisePsa_ApiPrivateKey}"))
 $headers = @{
     "Authorization" = "Basic $authHeader"
