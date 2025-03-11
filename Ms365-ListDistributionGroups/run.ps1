2690fb124723cb3c96983d879024e0b81ea4a08a
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-03-11 15:57:02 +0000

    Update run.ps1

--- Ms365-ListDistributionGroups/run.ps1
+++ Ms365-ListDistributionGroups/run.ps1
@@ -107,19 +107,16 @@ $credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365
 Connect-MgGraph -ClientSecretCredential $credential365 -TenantId $tenantId -NoWelcome
 
 # Get all distribution groups in the tenant
-$distributionGroups = Get-MgGroup -Filter "groupTypes/any(c:c eq 'Unified')" -All
+$distributionGroups = Get-DistributionGroup | Where-Object { $_.PrimarySmtpAddress -notlike "*.onmicrosoft.com" }
 
-# Filter out groups with .onmicrosoft.com addresses
-$filteredGroups = $distributionGroups | Where-Object { $_.Mail -notlike "*.onmicrosoft.com" }
+# Extract group names
+$groupNames = $distributionGroups | Select-Object -ExpandProperty DisplayName 
+$groupNames = $groupNames | Sort-Object
 
-# Extract group names and email addresses
-$groupDetails = $filteredGroups | Select-Object @{Name="GroupName";Expression={$_.DisplayName}}, @{Name="EmailAddress";Expression={$_.Mail}}
-$groupDetails = $groupDetails | Sort-Object GroupName
+# Convert the array of group names to a comma-separated string
+$groupNamesString = $groupNames -join ","
 
-# Convert the array of group details to a comma-separated string
-$groupDetailsString = $groupDetails | ForEach-Object { "$($_.GroupName) <$($_.EmailAddress)>" } -join ","
-
-Set-CloudRadialToken -Token "CompanyM365DistributionGroups" -AppId ${env:CloudRadialCsa_ApiPublicKey} -SecretId ${env:CloudRadialCsa_ApiPrivateKey} -CompanyId $companyId -GroupList $groupDetailsString
+Set-CloudRadialToken -Token "CompanyM365DistributionGroups" -AppId ${env:CloudRadialCsa_ApiPublicKey} -SecretId ${env:CloudRadialCsa_ApiPrivateKey} -CompanyId $companyId -GroupList $groupNamesString
 
 Write-Host "Updated CompanyM365DistributionGroups for Company Id: $companyId."
 
