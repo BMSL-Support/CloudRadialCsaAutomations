20fe6859cce02f9476f00babe2b5071346b49d84
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-03-17 10:55:09 +0000

    Update run.ps1

--- Ms365-ListGroups/run.ps1
+++ Ms365-ListGroups/run.ps1
@@ -117,7 +117,7 @@ $groupNames = $filteredGroups | Select-Object -ExpandProperty DisplayName
 $groupNames = $groupNames | Sort-Object
 
 # Convert the array of group names to a comma-separated string
-$groupNamesString = $groupNames -join ","
+$groupNamesString = if ($groupNames) { $groupNames -join "," } else { "No Security Groups available at this time." }
 
 Set-CloudRadialToken -Token "CompanyM365SecurityGroups" -AppId ${env:CloudRadialCsa_ApiPublicKey} -SecretId ${env:CloudRadialCsa_ApiPrivateKey} -CompanyId $companyId -GroupList $groupNamesString
 
