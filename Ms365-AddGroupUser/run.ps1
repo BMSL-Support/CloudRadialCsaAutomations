cf06772928cac8b58f6cef6cf0141c2d452cc8a7
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-04-10 10:22:54 +0100

    Update run.ps1

--- Ms365-AddGroupUser/run.ps1
+++ Ms365-AddGroupUser/run.ps1
@@ -25,14 +25,19 @@
     TenantId - string value of the tenant id, if blank uses the environment variable Ms365_TenantId
     TicketId - optional - string value of the ticket id used for transaction tracking
     SecurityKey - Optional, use this as an additional step to secure the function
+    LikeUserGroup - optional - email address or UPN of a user to list similar groups
+    LikeUserEmail - optional - email address or UPN of a user to list similar email groups
 
     JSON Structure
+
     {
         "UserPrincipalName": "user@domain.com",
         "GroupNames": ["Group Name 1", "Group Name 2"],
         "TenantId": "12345678-1234-1234-123456789012",
         "TicketId": "123456",
-        "SecurityKey": "optional"
+        "SecurityKey": "optional",
+        "LikeUserGroup": "likeuser@domain.com",
+        "LikeUserEmail": "likeuser@domain.com"
     }
 
 .OUTPUTS 
@@ -60,6 +65,8 @@ $GroupNames = $Request.Body.GroupNames
 $TenantId = $Request.Body.TenantId
 $TicketId = $Request.Body.TicketId
 $SecurityKey = $env:SecurityKey
+$LikeUserGroup = $Request.Body.LikeUserGroup
+$LikeUserEmail = $Request.Body.LikeUserEmail
 
 if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
     Write-Host "Invalid security key"
@@ -113,6 +120,36 @@ if ($resultCode -Eq 200)
     }
 
     $addedGroups = @()
+    $likeUserGroups = @()
+    $likeUserEmails = @()
+
+    if ($LikeUserGroup) {
+        $likeUserObject = Get-MgUser -Filter "userPrincipalName eq '$LikeUserGroup'"
+        if ($likeUserObject) {
+            $userGroups = Get-MgUserMemberOf -UserId $likeUserObject.Id | Where-Object { $_.DisplayName -like "Security -*" -or $_.DisplayName -like "Data -*" -or $_.DisplayName -like "SP Data -*" }
+            foreach ($group in $userGroups) {
+                New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $UserObject.Id
+                $likeUserGroups += $group.DisplayName
+            }
+        } else {
+            $message += "Request failed. LikeUserGroup `"$LikeUserGroup`" could not be found.`n"
+            $resultCode = 500
+        }
+    }
+
+    if ($LikeUserEmail) {
+        $likeUserObject = Get-MgUser -Filter "userPrincipalName eq '$LikeUserEmail'"
+        if ($likeUserObject) {
+            $distributionGroups = Get-MgGroup -Filter "mailEnabled eq true" -All
+            $filteredGroups = $distributionGroups | Where-Object { $_.groupTypes -notcontains 'Unified' -and $_.Mail -notlike "*.onmicrosoft.com" }
+            foreach ($group in $filteredGroups) {
+                $likeUserEmails += $group.DisplayName
+            }
+        } else {
+            $message += "Request failed. LikeUserEmail `"$LikeUserEmail`" could not be found.`n"
+            $resultCode = 500
+        }
+    }
 
     foreach ($GroupName in $GroupNames) {
         $GroupObject = Get-MgGroup -Filter "displayName eq '$GroupName'"
@@ -142,6 +179,14 @@ if ($resultCode -Eq 200)
 
     if ($addedGroups.Count -gt 0) {
         $message = "Added Groups:`n`n" + ($addedGroups -join "`n")
+    }
+
+    if ($likeUserGroups.Count -gt 0) {
+        $message += "`nAutomatically Assigned Groups:`n`n" + ($likeUserGroups -join "`n")
+    }
+
+    if ($likeUserEmails.Count -gt 0) {
+        $message += "`nGroups to be Manually Added in Exchange Online Management Portal:`n`n" + ($likeUserEmails -join "`n")
     }
 }
 
