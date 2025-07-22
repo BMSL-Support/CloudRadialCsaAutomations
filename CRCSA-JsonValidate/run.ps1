7ed5cd9254cb05cd513456018f93ce8adfee2784
BMSL-Support <122010409+BMSL-Support@users.noreply.github.com>
2025-07-22 09:16:18 +0100

    Create run.ps1

---
+++ CRCSA-JsonValidate/run.ps1
@@ -0,0 +1,57 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+function Clear-ObjectPlaceholders {
+    param ([psobject]$obj)
+
+    if ($obj -is [System.Collections.IDictionary]) {
+        $hasNull = $false
+        foreach ($key in $obj.Keys) {
+            $obj[$key] = Clear-ObjectPlaceholders -obj $obj[$key]
+            if ($null -eq $obj[$key]) { $hasNull = $true }
+        }
+        if ($hasNull) { return $null }
+    }
+    elseif ($obj -is [System.Collections.IEnumerable] -and -not ($obj -is [string])) {
+        if ($obj.Count -eq 1 -and $obj[0] -is [string] -and $obj[0] -match '^@') {
+            return @()
+        }
+        $newArray = @()
+        foreach ($item in $obj) {
+            $cleaned = Clear-ObjectPlaceholders -obj $item
+            if ($null -ne $cleaned) { $newArray += ,$cleaned }
+        }
+        return $newArray
+    }
+    elseif ($obj -is [string] -and $obj -match '^@') {
+        return $null
+    }
+    return $obj
+}
+
+# Read and parse the incoming JSON
+$body = Get-Content $Request.Body | Out-String
+try {
+    $jsonObj = $body | ConvertFrom-Json
+} catch {
+    $response = @{
+        status = 400
+        body = "Invalid JSON"
+    }
+    return Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::BadRequest
+        Body = "Invalid JSON"
+        Headers = @{ "Content-Type" = "text/plain" }
+    })
+}
+
+# Clean the JSON
+$cleaned = Clear-ObjectPlaceholders -obj $jsonObj
+
+# Return the cleaned JSON
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = ($cleaned | ConvertTo-Json -Depth 10)
+    Headers = @{ "Content-Type" = "application/json" }
+})
